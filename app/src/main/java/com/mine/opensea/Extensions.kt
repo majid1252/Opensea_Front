ca8c0c7914af470c508cfc1bfc5977961ea66570
majid <majid.jafari.iut@gmail.com>
2022-01-01 17:28:35 +0330

    Preparing some UI toolkits

--- app/src/main/java/com/mine/opensea/Extensions.kt
+++ app/src/main/java/com/mine/opensea/Extensions.kt
@@ -1,9 +1,30 @@
 package com.mine.opensea
 
+import android.widget.ImageView
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import android.graphics.drawable.BitmapDrawable
 
-public fun Observable<Any>.applyAndroidSchedulers(): Observable<Any> {
+import android.graphics.Bitmap
+import androidx.palette.graphics.Palette
+
+
+/**
+ * simply adding necessary android schedulers for [Observable]
+ */
+fun Observable<Any>.applyAndroidSchedulers(): Observable<Any> {
     return subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+}
+
+/**
+ * getting dominant color accent from [ImageView]
+ */
+fun ImageView.getDominantColor(): Int {
+    val bitmap = (drawable as BitmapDrawable).bitmap
+    var vibrant: Int = R.color.accent_color
+    Palette.from(bitmap).generate { palette ->
+        vibrant = palette?.getVibrantColor(0x000000)!! // <=== color you want
+    }
+    return vibrant
 }
